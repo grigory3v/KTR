f44199788828e6adb42f4f163716f3b30d199a29
Grigory3v <jack977j@gmail.com>
2022-08-07 15:05:31 +0400

    добавили функцию, которая принимает массив от пользователя и записывает его

--- task/Program.cs
+++ task/Program.cs
@@ -3,3 +3,21 @@
 строк формирует массив из строк,длина которых меньше либо равна 3 символа.
 */
 
+namespace KTR
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($" Введите длину массива");
+            int size = Convert.ToInt32(Console.ReadLine());
+            string?[] array = new string[size];
+            Console.WriteLine($"заполните массив, вводите значения массива по одному, после каждого числа нажимайте ENTER. ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToString(Console.ReadLine());
+
+            }
+        }
+    }
+}
